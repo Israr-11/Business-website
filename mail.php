8186268cd9619a06a3f25483902285cae88b1dd6
Israr <israrahmedpk1999@gmail.com>
2022-07-29 09:23:25 +0500

    first commit

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+  if(isset( $_POST['name']))
+  $name = $_POST['name'];
+  if(isset( $_POST['email']))
+  $email = $_POST['email'];
+  if(isset( $_POST['message']))
+  $message = $_POST['message'];
+  if(isset( $_POST['subject']))
+  $subject = $_POST['subject'];
+
+  $content="From: $name \n Email: $email \n Message: $message";
+  $recipient = "abdullahshabbirab0@gmail.com";
+  $mailheader = "From: $email \r\n";
+  mail($recipient, $subject, $content, $mailheader) or die("Error!");
+  echo "<script> alert('Email sent successfully'); </script>";
+  echo "<a href='index.html'>Go to Homepage</a>";
+?>
